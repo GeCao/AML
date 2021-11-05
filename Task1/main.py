355ac1b7413d8af597866254683a41188e0db82b
liyining100 <liyining100@gmail.com>
2021-11-05 13:20:48 +0100

    add lassoCV in feature selection

--- Task1/main.py
+++ Task1/main.py
@@ -14,15 +14,15 @@ if __name__ == "__main__":
                         choices=['zscore', 'isolationforest', 'else'], help='type of outlier')
     parser.add_argument('--pca', type=str,
                         default='tree',
-                        choices=['pca', 'tree', 'lasso', 'else'], help='type of feature selection')
+                        choices=['pca', 'tree', 'lsvc','lasso', 'else'], help='type of feature selection')
     parser.add_argument('--device', type=str, default='cuda', choices=['cuda', 'cpu'], help='type of device')
 
     args = parser.parse_args()
     model = args.model
     imputer = args.imputer
     outlier = args.outlier
-    pca = args.pca
-    device = args.device
+    pca = 'lasso'#args.pca
+    device = 'cpu'#args.device
 
     core_managemnet = CoreComponent(model=model, imputer=imputer, outlier=outlier, pca=pca, device=device)
     core_managemnet.initialization()
